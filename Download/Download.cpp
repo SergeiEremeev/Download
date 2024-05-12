1aad715676cc70a964f4c346534a1da27cc16ba3
User-11 <User-11@USER-11>
2024-05-12 19:36:55 +0300

    Изменения 1Изменения 1Добавьте файлы проекта.

---
+++ Download/Download.cpp
@@ -0,0 +1,19 @@
+﻿#include <windows.h> 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian"); // подключение русского языка
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    cout << "Hello World!\n";
+    cout << "Hello World!\n";
+    cout << "Hello World!\n";
+
+    cout << "Hello World!\n";
+    cout << "Hello World!\n";
+
+
+}
